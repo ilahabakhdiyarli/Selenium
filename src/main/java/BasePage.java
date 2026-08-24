e836b5c63db3fc884fa571b996d3f9e496571d4e
Ilaha <ilaha>
2026-08-24 20:38:07 +0400

    First commit

--- src/main/java/BasePage.java
+++ src/main/java/BasePage.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -27,9 +27,9 @@ public class BasePage {
 
     protected void scrollToElement(By locator) {
         WebElement element = waitForVisible(locator);
-        ((JavascriptExecutor) driver).executeScript(
-                "arguments[0].scrollIntoView({block: 'center'});", element);
+        new Actions(driver).moveToElement(element).perform();
     }
+
     protected void setValue(By locator, String value) {
         WebElement element = waitForVisible(locator);
         element.clear();
